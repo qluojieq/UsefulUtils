de1fb1b3c219073ac5d3e939efa2347dd3bb088e
jie.luo <904193639@qq.com>
2017-08-12 13:31:21 +0800

    delete log
    make it

--- Utils.java
+++ Utils.java
@@ -1,9 +1,4 @@
 /**
- * Copyright (C) 2015 Zhensheng Yongyida Robot Co.,Ltd. All rights reserved.
- * 
- * @author: hujianfeng@yongyida.com
- * @version 0.1
- * @date 2015-09-01
  * 
  */
 package robot.yongyida.com.omrondemo.utils;
